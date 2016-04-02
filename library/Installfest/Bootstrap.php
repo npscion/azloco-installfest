008870472c162826111ecee6c04f353a862a59eb
Nicholas Pscion <nicholas@whale.enterprises>
2016-04-02 10:21:52 -0700

    Cleaned up non-installfest code, updated Readme with more detailed information on setting up, and project purpose.

--- library/Installfest/Bootstrap.php
+++ library/Installfest/Bootstrap.php
@@ -124,64 +124,6 @@ class Installfest_Bootstrap
 
     public static function setupGlobals()
     {
-        /*$setting_mapper = Installfest_StaticLocator::getInstance()->getSettingMapper();
-
-        $setting_query = new Installfest_Model_QueryCondition();
-        $setting_query->setReturnType('associative_array');
-        $setting_query->setCache(true);
-        $setting_query->setCacheName('global_setting');
-        $settings = $setting_mapper->getResultSet($setting_query);
-
-        foreach ($settings as $setting) {
-            define(strtoupper($setting['code']), $setting['value']);
-        }
-        define('Installfest_default_S3_HTTPS_ADDRESS', 'https://Installfestauction.s3.amazonaws.com/');
-
-        define('RESERVE_USER_ID', 6835);
-        define('RETAIL_STORE_USER_ID', 2);
-        define('SYSTEM_USER_ID', 0);
-        define('SYSTEM_ADDRESS_ID', 0);
-
-        define('RESET_PASSWORD_MINS', 30);
-
-        // Auction constants
-        define('AUCTION_END_HOUR', 20);
-        define('AUCTION_END_MIN', 0);
-        define('DAILY_START_HOUR', 22);
-        define('DAILY_START_MIN', 0);
-        define('DAILY_END_HOUR', AUCTION_END_HOUR);
-        define('DAILY_END_MIN', AUCTION_END_MIN);
-        define('WEEKLY_START_HOUR', 20);
-        define('WEEKLY_START_MIN', 30);
-        define('WEEKLY_END_HOUR', AUCTION_END_HOUR);
-        define('WEEKLY_END_MIN', AUCTION_END_MIN);
-        define('MONTHLY_START_HOUR', 20);
-        define('MONTHLY_START_MIN', 30);
-        define('MONTHLY_END_HOUR', AUCTION_END_HOUR);
-        define('MONTHLY_END_MIN', AUCTION_END_MIN);
-        define('DAILY_START_PRICE', 1.00);
-        define('WEEKLY_RESERVE_ACTIVATION_DAYS', 3);
-        define('MONTHLY_RESERVE_ACTIVATION_DAYS', 7);
-        define('RESERVE_ACTIVATION_HOUR', 9);
-        define('RESERVE_ACTIVATION_MIN', 0);
-        define('MAX_RETAIL_ID', 662);
-
-        define("AUTHORIZENET_API_LOGIN_ID", self::$config->authorize->api_id);
-        define("AUTHORIZENET_TRANSACTION_KEY", self::$config->authorize->transaction_key);
-        define("AUTHORIZENET_SANDBOX", self::$config->authorize->sandbox);
-
-        define("MAILCHIMP_API_KEY", self::$config->mailchimp->api_key);
-        define("MAILCHIMP_TEST_LIST_ID", self::$config->mailchimp->test_list_id);
-        define("MAILCHIMP_Installfest_LIST_ID", self::$config->mailchimp->Installfest_list_id);
-        define("MAILCHIMP_NATIONALS_LIST_ID", self::$config->mailchimp->nationals_list_id);
-
-        define("TROPO_TEXT", "text");
-        define("TROPO_VOICE", "voice");
-        define("TROPO_API_URL", self::$config->tropo->api_url);
-        define("TROPO_TEXT_TOKEN", self::$config->tropo->token->text);
-        define("TROPO_VOICE_TOKEN", self::$config->tropo->token->voice);
-
-        define("MANDRILL_API_KEY", self::$config->mandrill->api_key);*/
         define("INSTALLFEST_ROOT_URL", self::$config->site->url);
     }
 
